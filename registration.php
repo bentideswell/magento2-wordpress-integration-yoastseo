31d6c4977ff235da1be8651b8bb0b1763c5294f3
Ben Tideswell <ben@fishpig.co.uk>
2017-03-11 08:03:37 +0000

    1.0.0.3

--- registration.php
+++ registration.php
@@ -1,13 +1,31 @@
 <?php
 /**
- * FishPig_WordPress_Yoast
- * http://fishpig.co.uk/
+ * FishPig_WordPress_Yoast by Ben Tideswell
+ * https://fishpig.co.uk/
  */
 	
 	use \Magento\Framework\Component\ComponentRegistrar;
-	
+
 	ComponentRegistrar::register(
 		ComponentRegistrar::MODULE,
 		'FishPig_WordPress_Yoast',
 		__DIR__
 	);
+
+	/*
+	 * Protection against installing module in incorrect directory
+	**/
+	$currentLocation = __DIR__;
+
+	if (strpos($currentLocation, 'app' . DIRECTORY_SEPARATOR . 'code' . DIRECTORY_SEPARATOR) !== false) {
+		$relativeLocation = 'app' . DIRECTORY_SEPARATOR . 'code' . DIRECTORY_SEPARATOR . 'FishPig' . DIRECTORY_SEPARATOR . 'WordPress_Yoast';
+		
+		if (strpos($currentLocation, $relativeLocation) === false) {
+			throw new \Exception(sprintf(
+				"%s is installed in the wrong folder. Please install the module in %s and make sure that you use the correct capitalisation of the module name (%s).",
+				"FishPig_WordPress_Yoast",
+				$relativeLocation,
+				'FishPig' . DIRECTORY_SEPARATOR . 'WordPress_Yoast'
+			));
+		}
+	}
