7d78fbd2825af739de00d3d5c4787e59384fbd64
Ben Tideswell <ben@fishpig.co.uk>
2019-01-26 13:14:38 +0000

    2.0.0.7

---
+++ Block/Adminhtml/System/Config/Form/Field/Addon.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ *
+ */
+namespace FishPig\WordPress_Yoast\Block\Adminhtml\System\Config\Form\Field;
+
+/* Parent Class */
+use Magento\Config\Block\System\Config\Form\Field;
+
+/* Misc */
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Addon extends Field
+{
+	/**
+	 *
+	 *
+	 * @param  AbstractElement $element
+	 * @return string
+	 */
+	protected function _getElementHtml(AbstractElement $element)
+	{
+		return '<span style="display: block;border: 1px solid #139b13;background: #e5f6e5;line-height:1em;padding:10px;font-size:13px; color:#04260d;">Version ' . $this->getModuleVersion() . '</span>';
+	}
+
+	/**
+	 *
+	 *
+	 * @param  AbstractElement $element
+	 * @return string
+	 */
+	public function render(AbstractElement $element)
+	{
+		return $this->_getElementHtml($element);
+	}
+}
