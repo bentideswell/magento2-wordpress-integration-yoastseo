c075c0a468cf46a8296f2a69148a9bf67f94977b
Ben Tideswell <ben@fishpig.co.uk>
2019-04-16 13:23:22 +0100

    2.0.0.11

--- Block/Adminhtml/System/Config/Form/Field/Addon.php
+++ Block/Adminhtml/System/Config/Form/Field/Addon.php
@@ -20,7 +20,7 @@ class Addon extends Field
 	 */
 	protected function _getElementHtml(AbstractElement $element)
 	{
-		return '<span style="display: block;border: 1px solid #139b13;background: #e5f6e5;line-height:1em;padding:10px;font-size:13px; color:#04260d;">Version ' . $this->getModuleVersion() . '</span>';
+		return '<span style="display: block; border: 1px solid #ccc; background: #f6f6f6; line-height:1em; padding:10px; font-size:13px; color:#04260d;">Version ' . $this->getModuleVersion() . '</span>';
 	}
 
 	/**
